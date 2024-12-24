df7fd02511fe179c7569b1c0d4dd27a32006b0fc
freedom-pep <charlesupjohn@Charless-MacBook-Pro.local>
2024-12-24 10:55:46 -0500

    Clean up test name

--- rust_code/src/serial.rs
+++ rust_code/src/serial.rs
@@ -45,19 +45,44 @@ mod tests {
                 [
                   "start",
                   "2024-12-22 21:49:04.082835 UTC"
+                ],
+                [
+                  "stop",
+                  "2024-12-22 22:49:04.082835 UTC"
+                ]
+              ],
+              "2024-12-24": [
+                [
+                  "start",
+                  "2024-12-24 01:00:04.082835 UTC"
                 ]
               ]
             }"#;
 
-        let mut base_hash_map = HashMap::new();
-        let zz = vec![SerEntry::new(
+        let mut base_data = HashMap::new();
+        let start_1 = SerEntry::new(
             "start".to_string(),
             "2024-12-22 21:49:04.082835 UTC"
                 .parse::<DateTime<Utc>>()
                 .unwrap(),
-        )];
-        base_hash_map.insert("2024-12-22".to_string(), zz);
-        let expected = SerData::new(base_hash_map);
+        );
+        let stop_1 = SerEntry::new(
+            "stop".to_string(),
+            "2024-12-22 22:49:04.082835 UTC"
+                .parse::<DateTime<Utc>>()
+                .unwrap(),
+        );
+        let start_2 = SerEntry::new(
+            "start".to_string(),
+            "2024-12-24 01:00:04.082835 UTC"
+                .parse::<DateTime<Utc>>()
+                .unwrap(),
+        );
+        let twenty_second = vec![start_1, stop_1];
+        let twenty_fourth = vec![start_2];
+        base_data.insert("2024-12-22".to_string(), twenty_second);
+        base_data.insert("2024-12-24".to_string(), twenty_fourth);
+        let expected = SerData::new(base_data);
         let result: SerData = serde_json::from_str(data).unwrap();
         assert_eq!(result, expected)
     }
